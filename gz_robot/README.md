b81e76a1fbd3dd5dab6ddde849c1022e2309ef6c
swagatk <swagat.kumar@gmail.com>
2025-02-23 21:42:34 +0000

    ros2 gazebo launch file

--- gz_robot/README.md
+++ gz_robot/README.md
@@ -17,5 +17,24 @@ sudo apt install ros-jazzy-gz-launch-vendor* -y
 gz launch avoid_obstacle.gzlaunch
 ```
 
+## Launch Gazebo using ROS2 and use `ros_gz_bridge` to access gazebo topics
+```
+ros2 launch gz_robot.launch.py
+```
+Open a terminal and list the topics as:
+```
+ros2 topic list
+```
+Open another terminal and provide command to the robot:
+```
+ros2 topic pub /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 1.8}}"
+
+```
+You can see that the robot is moving in a circle.  Also you can check the sensor output by using the
+following command:
+
+```
+ros2 topic echo /lidar
+```
 ## Notes
 Press the play button (on button left part of Gazebo window) to see the robot moving
---
+++ gz_robot/gz_robot.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    ros_gz_sim_pkg_path = get_package_share_directory('ros_gz_sim')
+    gz_launch_path = PathJoinSubstitution([ros_gz_sim_pkg_path, 'launch', 'gz_sim.launch.py'])
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(gz_launch_path),
+            launch_arguments={
+                'gz_args': 'robot_world.sdf',  # Replace with your own world file
+                'on_exit_shutdown': 'True'
+            }.items(),
+        ),
+
+        # Bridging and remapping Gazebo topics to ROS 2 (replace with your own topics)
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=['/imu@sensor_msgs/msg/Imu@gz.msgs.IMU',
+                       '/lidar@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
+                       '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist'],
+            output='screen'
+        ),
+    ])
